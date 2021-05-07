e12aa126092674c5553ea3f5b3477ce7113c3380
omen <1094978730@qq.com>
2021-05-07 23:06:50 +0800

    ''

--- clinet80/src/main/java/com/cxb/controller/Controlller80.java
+++ clinet80/src/main/java/com/cxb/controller/Controlller80.java
@@ -2,8 +2,11 @@ package com.cxb.controller;
 
 import com.cxb.entity.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * @author omen
@@ -14,8 +17,10 @@ import org.springframework.web.client.RestTemplate;
 public class Controlller80 {
 
     private static final String URL = "http://localhost:8001";
+    @Resource
     private RestTemplate restTemplate;
 
+    @GetMapping("/hello")
     public R hello() {
         return restTemplate.getForObject(URL+"/hello",R.class);
     }
