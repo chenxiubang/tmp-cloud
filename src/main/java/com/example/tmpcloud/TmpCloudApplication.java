d6df2f009dc569c26036771856a8bda6a676c438
omen <1094978730@qq.com>
2021-05-07 21:46:15 +0800

    Initial commit

---
+++ src/main/java/com/example/tmpcloud/TmpCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.tmpcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmpCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmpCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tmpcloud/TmpCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmpcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmpCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
